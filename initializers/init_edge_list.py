c2d8913036f3ca48465aa45f8028010b5a436191
arden1601 <corneliusardensatwikahermawan@mail.ugm.ac.id>
2024-06-02 21:42:50 +0700

    feat: add double edges function and add all maps

--- initializers/init_edge_list.py
+++ initializers/init_edge_list.py
@@ -1,74 +1,134 @@
-edge_list = [{
+# edge_list = [{
+# 		'edge': (1, 3),
+# 		'weight': 1,
+# 		'width': 2
+# 		},{
+# 		'edge': (2, 3),
+# 		'weight': 1,
+# 		'width': 2
+# 		},{
+# 		'edge': (3, 4),
+# 		'weight': 1,
+#   	'width': 2
+# 		},{
+# 		'edge': (4, 5),
+# 		'weight': 1,
+#   	'width': 2
+# 		},{
+# 		'edge': (5, 6),
+# 		'weight': 1,
+#   	'width': 2
+# 		},{
+# 		'edge': (3, 7),
+# 		'weight': 10,
+#   	'width': 2
+# 		},{
+# 		'edge': (8, 7),
+# 		'weight': 1,
+#   	'width': 2
+# 		},{
+# 		'edge': (6, 8),
+# 		'weight': 1,
+#   	'width': 2
+# 		},{
+# 		'edge': (7, 11),
+# 		'weight': 1,
+#   	'width': 2
+# 		},{
+# 		'edge': (11, 12),
+# 		'weight': 1,
+#   	'width': 2
+# 		},{
+# 		'edge': (12, 10),
+# 		'weight': 1,
+#   	'width': 2
+# 		},{
+# 		'edge': (7, 8),
+# 		'weight': 1,
+#   	'width': 2
+# 		},{
+# 		'edge': (10, 12),
+# 		'weight': 1,
+#   	'width': 2
+# 		},{
+# 		'edge': (11, 7),
+# 		'weight': 1,
+#   	'width': 2
+# 		},{
+# 		'edge': (10, 13),
+# 		'weight': 1,
+#   	'width': 2
+# 		},{
+# 		'edge': (13, 9),
+# 		'weight': 1,
+#   	'width': 2
+# 		},{
+# 		'edge': (9, 14),
+# 		'weight': 1,
+#   	'width': 2
+# 		},{
+# 		'edge': (8, 14),
+# 		'weight': 1,
+#   	'width': 1
+# 		}
+#   ]
+
+
+edge_list = [
+	{
 		'edge': (1, 3),
 		'weight': 1,
 		'width': 2
-		},{
+	},
+	{
+		'edge': (3, 1),
+		'weight': 1,
+		'width': 2
+	},
+	{
 		'edge': (2, 3),
 		'weight': 1,
 		'width': 2
-		},{
+	},
+	{
+		'edge': (3, 2),
+		'weight': 1,
+		'width': 2
+	},
+	{
+		'edge': (4, 3),
+		'weight': 1,
+		'width': 2
+	},
+	{
 		'edge': (3, 4),
 		'weight': 1,
-  	'width': 2
-		},{
+		'width': 2
+	},
+	{
+		'edge': (5, 4),
+		'weight': 1,
+		'width': 2
+	},
+	{
 		'edge': (4, 5),
 		'weight': 1,
-  	'width': 2
-		},{
-		'edge': (5, 6),
+		'width': 2
+	},
+	{
+		'edge': (1, 6),
 		'weight': 1,
-  	'width': 2
-		},{
-		'edge': (3, 7),
-		'weight': 10,
-  	'width': 2
-		},{
-		'edge': (8, 7),
+		'width': 2
+	},
+	{
+		'edge': (6, 7),
 		'weight': 1,
-  	'width': 2
-		},{
-		'edge': (6, 8),
+		'width': 2
+	},
+	{
+		'edge': (7, 6),
 		'weight': 1,
-  	'width': 2
-		},{
-		'edge': (7, 11),
-		'weight': 1,
-  	'width': 2
-		},{
-		'edge': (11, 12),
-		'weight': 1,
-  	'width': 2
-		},{
-		'edge': (12, 10),
-		'weight': 1,
-  	'width': 2
-		},{
-		'edge': (7, 8),
-		'weight': 1,
-  	'width': 2
-		},{
-		'edge': (10, 12),
-		'weight': 1,
-  	'width': 2
-		},{
-		'edge': (11, 7),
-		'weight': 1,
-  	'width': 2
-		},{
-		'edge': (10, 13),
-		'weight': 1,
-  	'width': 2
-		},{
-		'edge': (13, 9),
-		'weight': 1,
-  	'width': 2
-		},{
-		'edge': (9, 14),
-		'weight': 1,
-  	'width': 2
-		},{
-		'edge': (8, 14),
-		'weight': 1,
-  	'width': 1
-		}
-  ]
+		'width': 2
+	},
+]
+
