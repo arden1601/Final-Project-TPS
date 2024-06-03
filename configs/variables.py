772942a1f5ddbb3500c8f09493c4248ad533037c
iZcy <yitzhakedmundtiomanalu@mail.ugm.ac.id>
2024-06-03 10:59:14 +0700

    fix: enable debug

--- configs/variables.py
+++ configs/variables.py
@@ -3,16 +3,16 @@ show_opt = ['quota', 'vehicles', 'weight']
 show = show_opt[-1]
 
 # Show the boxes to vehicles?
-show_boxes = False
+show_boxes = True
 
 # Show the busy nodes?
 show_busy = True
 
 # Show the road?
-show_road = True
+show_road = False
 
 # Show node boxes?
-show_node_boxes = False
+show_node_boxes = True
 
 # Show the clock
 show_clock = True
